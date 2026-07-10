1ebbb8805b4254cc03fa587d3928d2c155671918
Luca Schumacher <l.schumacher@luftiger.eu>
2026-07-10 02:34:14 +0200

    feat: add middleware for error recovery and logging

--- main.go
+++ main.go
@@ -29,7 +29,23 @@ func main() {
 	mux.HandleFunc("/api/create", urlCtrl.CreateShortUrl)
 	mux.HandleFunc("/api/delete", urlCtrl.DeleteShortUrl)
 
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	if err := http.ListenAndServe(":8080", middleware(mux)); err != nil {
 		slog.Error("Could not start the webserver:", "error", err.Error())
 	}
+}
+
+func middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		defer func() {
+			err := recover()
+			if err != nil {
+				slog.Warn("web middleware recovery caught smth", "error", err)
+				http.Error(w, "There was an internal server error", http.StatusInternalServerError)
+			}
+		}()
+
+		slog.Info(r.URL.Path + " called from " + r.RemoteAddr)
+		next.ServeHTTP(w, r)
+	})
 }
